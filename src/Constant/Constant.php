c3a100fccacfe62aa3a268833e6bf14d02f4dbd5
passioneight <michael.cuenez@passioneight.at>
2020-06-15 13:11:38 +0200

    + Added generic Constant class

---
+++ src/Constant/Constant.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Passioneight\Bundle\PhpUtilitiesBundle\Constant;
+
+use Exception;
+use ReflectionClass;
+
+abstract class Constant
+{
+    /**
+     * @return array all constants in the current class
+     */
+    public static function getAll()
+    {
+        try {
+            $class = new ReflectionClass(get_called_class());
+            return $class->getConstants();
+        } catch (Exception $e) {
+            return [];
+        }
+    }
+
+    /**
+     * @param string $name the name to look for
+     *
+     * @return bool whether the constant with the given name is available
+     */
+    public static function has($name)
+    {
+        $constants = self::getAll();
+        return in_array($name, $constants);
+    }
+
+    /**
+     * Use the self#has method to check whether the constant with the name exists.
+     *
+     * @param string $name the name to look for
+     *
+     * @return mixed the value of the constant or the value of $name
+     */
+    public static function get($name)
+    {
+        $constants = self::getAll();
+        return @$constants[strtoupper($name)] ?: $name;
+    }
+
+    /**
+     * @param string $input
+     * @param string $separator
+     * @return string the camel-cased string.
+     */
+    public static function toCamelCase(string $input, $separator = '-')
+    {
+        return str_replace($separator, '', ucwords($input, $separator));
+    }
+}
