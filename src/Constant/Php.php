87cb183e40270ef8ead577170d11780a6f09d854
passioneight <michael.cuenez@passioneight.at>
2020-06-15 13:10:03 +0200

    + Added convenience constants for PHP

---
+++ src/Constant/Php.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Passioneight\Bundle\PhpUtilitiesBundle\Constant;
+
+class Php
+{
+    const NAMESPACE_DELIMITER = "\\";
+    const METHOD_TYPE_SETTER = "set";
+    const METHOD_TYPE_GETTER = "get";
+    const METHOD_TYPE_ISSER = "is";
+}
