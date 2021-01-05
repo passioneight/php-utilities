cfed8e80e75b78fae847c6402198cef56502a451
passioneight <michael.cuenez@passioneight.at>
2021-01-05 19:53:52 +0100

    ~ Added some more convenience methods to MethodUtility

--- src/Service/Utility/MethodUtility.php
+++ src/Service/Utility/MethodUtility.php
@@ -8,6 +8,57 @@ class MethodUtility
 {
     /**
      * @param string $name
+     * @param int $offset
+     * @return bool
+     */
+    public static function isGetter(string $name, int $offset = 0)
+    {
+        return self::is($name,MethodType::GET, $offset);
+    }
+
+    /**
+     * @param string $name
+     * @param int $offset
+     * @return bool
+     */
+    public static function isSetter(string $name, int $offset = 0)
+    {
+        return self::is($name,MethodType::SET, $offset);
+    }
+
+    /**
+     * @param string $name
+     * @param int $offset
+     * @return bool
+     */
+    public static function isIsser(string $name, int $offset = 0)
+    {
+        return self::is($name,MethodType::IS, $offset);
+    }
+
+    /**
+     * @param string $name
+     * @param int $offset
+     * @return bool
+     */
+    public static function isHasser(string $name, int $offset = 0)
+    {
+        return self::is($name,MethodType::HAS, $offset);
+    }
+
+    /**
+     * @param string $name
+     * @param string $type
+     * @param int $offset
+     * @return bool
+     */
+    public static function is(string $name, string $type, int $offset = 0)
+    {
+        return substr($name, $offset, strlen($type)) === $type;
+    }
+
+    /**
+     * @param string $name
      * @return string
      */
     public static function createGetter(string $name)
