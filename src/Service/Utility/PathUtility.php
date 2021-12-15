5f52e436e39caef0003d223e11f0bd4eb7fc2d02
passioneight <michael.cuenez@passioneight.at>
2021-12-15 10:18:05 +0100

    Added path utility to ensure a path exists and to get the path from a file

--- src/Service/Utility/PathUtility.php
+++ src/Service/Utility/PathUtility.php
@@ -30,4 +30,28 @@ class PathUtility
     {
         return DIRECTORY_SEPARATOR . $path;
     }
+
+    /**
+     * @param string $path
+     * @param int $mode
+     * @param bool $recursive
+     * @return bool
+     */
+    public static function ensurePath(string $path, int $mode = 0777, bool $recursive = true)
+    {
+        if (!is_dir($path)) {
+            return mkdir($path, $mode, $recursive);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $file
+     * @return false|string
+     */
+    public static function getPathFromFile(string $file)
+    {
+        return StringUtility::removeFromEnd(DIRECTORY_SEPARATOR, $file, true);
+    }
 }
