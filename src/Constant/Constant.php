0f9d5c45548dd23400e18720829e571e5e7dbe1c
passioneight <michael.cuenez@mindspace.at>
2022-06-06 09:29:11 +0200

    chore: add type hinting
    resolves #4

--- src/Constant/Constant.php
+++ src/Constant/Constant.php
@@ -10,7 +10,7 @@ abstract class Constant
     /**
      * @return array all constants in the current class
      */
-    public static function getAll()
+    public static function getAll(): array
     {
         try {
             $class = new ReflectionClass(get_called_class());
@@ -21,11 +21,11 @@ abstract class Constant
     }
 
     /**
-     * @param string $name the name to look for
+     * @param mixed $name the name to look for
      *
      * @return bool whether the constant with the given name is available
      */
-    public static function has($name)
+    public static function has(mixed $name): bool
     {
         $constants = self::getAll();
         return in_array($name, $constants);
@@ -34,11 +34,11 @@ abstract class Constant
     /**
      * Use the self#has method to check whether the constant with the name exists.
      *
-     * @param string $name the name to look for
+     * @param string|null $name the name to look for
      *
      * @return mixed the value of the constant or the value of $name
      */
-    public static function get($name)
+    public static function get(?string $name): mixed
     {
         $constants = self::getAll();
         return @$constants[strtoupper($name)] ?: $name;
